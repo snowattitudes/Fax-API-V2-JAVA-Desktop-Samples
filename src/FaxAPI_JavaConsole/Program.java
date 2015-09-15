6d8714fcd72e09a97f5e9634d9f7dcd93a4cef5d
Snow Attitudes <chefarchitekt@live.com>
2015-09-15 13:10:39 +0800

    Now sending for both models

--- src/FaxAPI_JavaConsole/Program.java
+++ src/FaxAPI_JavaConsole/Program.java
@@ -5,16 +5,16 @@ import java.util.*;
 public class Program {
 
 	public static void main(String[] args) {
-		//FaxOperations faxSimpleModel = new FaxOperations();
+		FaxOperations faxSimpleModel = new FaxOperations();
 		FaxOperations faxCompleModel = new FaxOperations();
 
-		//System.out.println("Sending Fax Simple Model ...");
-		//try {
-			//faxSimpleModel.SendFaxSimpleModel();
-		//} catch (UnsupportedEncodingException e) {
-			// TODO Auto-generated catch block
-			//e.printStackTrace();
-		//}
+		System.out.println("Sending Fax Simple Model ...");
+		try {
+			faxSimpleModel.SendFaxSimpleModel();
+		} catch (UnsupportedEncodingException e) {
+			//TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		System.out.println("Enter any key to Send Fax Complex Model");
 		//new Scanner(System.in).nextLine();
 		System.out.println("Sending Fax Complex Model ...");
